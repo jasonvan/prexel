8f8d7e0c3f69e17ff67746a78e376c09e7cdb413
Jason Van Schooneveld <jvanschooneveld@gmail.com>
2017-12-11 12:56:22 +0800

    Updated Diagram model to handle merge

--- models/diagram.py
+++ models/diagram.py
@@ -24,6 +24,49 @@ class Diagram:
     def add_methods(self, methods):
         self.methods.extend(methods)
 
+    def merge(self, other):
+        if self != other:
+            return
+
+        # Add fields
+        self.fields.extend(other.fields)
+
+        # Add methods
+        self.methods.extend(other.methods)
+
+        # Subclasses TODO could be cleaned up
+        if other.sub_classes:
+            to_append = []
+
+            for other_sub_class in other.sub_classes:
+                for sub_class in self.sub_classes:
+                    if other_sub_class == sub_class:
+                        sub_class.merge(other_sub_class)
+                    else:
+                        if other_sub_class not in to_append:
+                            to_append.append(other_sub_class)
+
+            self.sub_classes.extend(to_append)
+
+        # Aggregated classes TODO could be cleaned up
+        if other.aggregated_classes:
+            to_append = []
+
+            for other_aggregated_class in other.aggregated_classes:
+                for aggregated_class in self.aggregated_classes:
+                    if other_aggregated_class == aggregated_class:
+                        # TODO need to deal with multiplicity, instance_name
+                        # etc
+                        aggregated_class.merge(other_aggregated_class)
+                    else:
+                        if other_aggregated_class not in to_append:
+                            to_append.append(other_aggregated_class)
+
+            self.aggregated_classes.extend(to_append)
+
+    def __eq__(self, other):
+        return self.name == other.name
+
 
 class AggregationDiagram(Diagram):
     def __init__(self, name=None, instance_name=None,
--- tests/test_diagram.py
+++ tests/test_diagram.py
@@ -36,6 +36,134 @@ class TestDiagram(unittest.TestCase):
         self.kitchen.add_method("method_1()")
         self.assertIn("method_1()", self.kitchen.methods)
 
+    def test__eg__(self):
+        self.assertTrue(self.kitchen == Diagram(name="Kitchen"))
+        self.assertFalse(self.kitchen == Diagram(name="kitchen"))
+
+    def test_merge_fields(self):
+        to_merge = Diagram(name="Kitchen", fields=["field1", "field2"])
+        self.kitchen.merge(to_merge)
+        self.assertEqual(self.kitchen.fields, ["field1", "field2"])
+
+    def test_merge_methods(self):
+        to_merge = Diagram(name="Kitchen",
+                           methods=["method_1()", "method_2()"])
+        self.kitchen.merge(to_merge)
+        self.assertEqual(self.kitchen.methods, ["method_1()", "method_2()"])
+
+    def test_merge_subclasses(self):
+        parent_class_1 = Diagram(name="Kitchen",
+                                 fields=[
+                                     "field_1",
+                                     "field_2",
+                                 ])
+
+        parent_class_2 = Diagram(name="Kitchen",
+                                 fields=[
+                                     "field_3",
+                                     "field_4",
+                                 ])
+
+        sub_class_1 = Diagram(name="FancyKitchen",
+                              fields=[
+                                  "field_5",
+                                   "field_6",
+                              ])
+
+        sub_sub_class_1 = Diagram(name="SuperFancyKitchen",
+                                  fields=[
+                                      "field_9",
+                                      "field_10",
+                                  ])
+
+        sub_class_2 = Diagram(name="FancyKitchen",
+                              fields=[
+                                  "field_7",
+                                  "field_8",
+                              ])
+
+        unique_sub_class_2 = Diagram(name="ModernKitchen",
+                                     fields=[
+                                         "unique_field_1",
+                                         "unique_field_2",
+                                     ])
+
+        sub_sub_class_2 = Diagram(name="SuperFancyKitchen",
+                                  fields=[
+                                      "field_11",
+                                      "field_12",
+                                  ])
+
+        sub_class_1.add_sub_class(sub_sub_class_1)
+        sub_class_2.add_sub_class(sub_sub_class_2)
+        parent_class_1.add_sub_class(sub_class_1)
+        parent_class_2.add_sub_class(sub_class_2)
+        parent_class_2.add_sub_class(unique_sub_class_2)
+        parent_class_1.merge(parent_class_2)
+
+        self.assertEqual(parent_class_1.fields,
+                         ["field_1",
+                          "field_2",
+                          "field_3",
+                          "field_4",
+                          ])
+
+        self.assertEqual(parent_class_1.sub_classes[0].fields,
+                         ["field_5",
+                          "field_6",
+                          "field_7",
+                          "field_8",
+                          ])
+
+        self.assertEqual(parent_class_1.sub_classes[1].fields,
+                         ["unique_field_1",
+                          "unique_field_2",
+                          ])
+
+        self.assertEqual(parent_class_1.sub_classes[0].sub_classes[0].fields,
+                         ["field_9",
+                          "field_10",
+                          "field_11",
+                          "field_12",
+                          ])
+
+    def test_merge_aggregated_classes(self):
+        class_1 = Diagram(name="Kitchen", fields=["field_1", "field_2", ])
+        class_2 = Diagram(name="Kitchen", fields=["field_3", "field_4", ])
+
+        aggregated_class_1 = Diagram(name="Cupboard",
+                                     fields=["field_5", "field_6", ])
+
+        aggregated_class_2 = Diagram(name="Cupboard",
+                                     fields=["field_7", "field_8", ])
+
+        aggregated_class_3 = Diagram(name="Door",
+                                     fields=["field_9", "field_10", ])
+
+        class_1.add_aggregated_class(aggregated_class_1)
+        class_2.add_aggregated_class(aggregated_class_2)
+        class_2.add_aggregated_class(aggregated_class_3)
+        class_1.merge(class_2)
+
+        self.assertEqual(class_1.fields,
+                         ["field_1",
+                          "field_2",
+                          "field_3",
+                          "field_4",
+                          ])
+
+        self.assertEqual(class_1.aggregated_classes[0].name, "Cupboard")
+        self.assertEqual(class_1.aggregated_classes[0].fields,
+                         ["field_5",
+                          "field_6",
+                          "field_7",
+                          "field_8", ])
+
+        self.assertEqual(class_1.aggregated_classes[1].name, "Door")
+        self.assertEqual(class_1.aggregated_classes[1].fields,
+                         ["field_9",
+                          "field_10", ])
+
 
 class TestAggregationDiagram(unittest.TestCase):
     def setUp(self):
@@ -47,6 +175,7 @@ class TestAggregationDiagram(unittest.TestCase):
                                            self.right_multi)
 
     def test_init(self):
+        # TODO FIXME
         self.assertEqual(self.cupboard.name, "Cupboard")
         self.assertEqual(self.cupboard.left_multiplicity, self.left_multi)
         self.assertEqual(self.cupboard.right_multiplicity, self.right_multi)
