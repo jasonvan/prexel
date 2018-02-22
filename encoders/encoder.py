4cb5464334e59fe530e2ede2b59f579917f96537
Nitish <32949680+nishgaddam@users.noreply.github.com>
2018-02-22 18:11:39 -0500

    added comments to encoder.py

--- encoders/encoder.py
+++ encoders/encoder.py
@@ -1,6 +1,6 @@
 from abc import ABCMeta, abstractmethod
 
-
+#Basically Encoder is an interface, we cannot create an instance of it, only use it as a superclass
 class Encoder(metaclass=ABCMeta):
     @abstractmethod
     def generate(self, diagram):
